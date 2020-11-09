0d82fae49314d96d05c528a9db0f05630e5386f3
The Prof <52168158+computationalknowledge@users.noreply.github.com>
2020-11-09 11:23:41 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Map from "./Map";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<Map />, rootElement);
